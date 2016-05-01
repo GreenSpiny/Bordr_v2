195d1b1aaffd3cc3f96652616056678d5aec3ece
Conrad Mossl <conradmossl@gmail.com>
2016-04-30 23:11:50 -0400

    Weird fixes

--- Components/Resources/js/index.js
+++ Components/Resources/js/index.js
@@ -11,8 +11,12 @@ function($scope, $http) {
   $scope.logInClick = function() {
     var data = {username: $scope.user, password: $scope.password};
     $.post('http://localhost:3000/login', data).done(function(response) {
+      console.log(response);
+      console.log(1);
       if (response.slice(0,11) == 'User Found:') 
+        console.log(2);
       if (response == 'Login Successful') {
+        console.log(3);
         goToPage($('#loginBTN').attr("value"));
       }
     });
@@ -53,53 +57,3 @@ function($scope, $http) {
     }
   }
 });
-
-// var signup = false;
-// $(document).ready( function() {
-//   $("#signupBTN").click( function() {
-//     if (!signup) {
-//       signup = true;
-//       $("#signUpInfo").slideDown();
-//       return;
-//     }
-//     else {
-//       var data = {username: $('#user').val(), password: $('#password').val(), confirm: $('#confirm').val(), email: $('#email').val()};
-//       console.log(data);
-//       $.post('http://localhost:3000/signup', data).done(function(response) {
-//         var alerts = "";
-//          if (response.username != null) {
-//           $('#user').addClass("error");
-//           $('#user').after('<div class="alert alert-warning">'+ response.username +'</div>');
-
-//         }
-//         if (response.password != null) {
-//           alerts += response.password + "\n";
-//           $('#password').addClass("error");
-//           $('#password').after('<div class="alert alert-warning">'+ response.password +'</div>');
-//         }
-//         if (response.confirm != null) {
-//           alerts += response.confirm + "\n";
-//           $('#password').addClass("error");
-//           $('#confirm').addClass("error");
-//           $('#confirm').after('<div class="alert alert-warning">'+ response.confirm +'</div>');
-//         }
-//         if (response.email != null) {
-//           alerts += response.email + "\n";
-//           $('#email').addClass("error");
-//           $('#email').after('<div class="alert alert-warning">'+ response.email +'</div>');
-//         }
-
-//         console.log("response",response);
-//       });
-//     }
-//   })  
-//   $("#loginBTN").click( function() {
-//     var data = {username: $('#user').val(), password: $('#password').val()};
-//     $.post('http://localhost:3000/login', data).done(function(response) {
-//       if (response.slice(0,11) == 'User Found:') 
-//       if (response == 'Login Successful') {
-//         goToPage($('#loginBTN').attr("value"));
-//       }
-//     });
-//   })  
-// });

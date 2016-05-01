a9e661af8cff47318fecb9f7d03bae55b10b1de2
Adam Kruchkow <akruchkow@gmail.com>
2016-05-01 17:17:52 -0400

    More events page, beginning of dynamic population

--- Components/Resources/js/transitions.js
+++ Components/Resources/js/transitions.js
@@ -67,6 +67,9 @@ function goToPage(num) {
   
   // Set page number to current page
   page = num;
+  
+  // TESTING
+  $("#eventsArea").html(makeEventDiv("Albert's Party","30 years!","skydiving","false"));
 }
 
 function scroll(direction, duration) {
@@ -81,4 +84,50 @@ function scroll(direction, duration) {
       $("#header").animate({top: 0}, duration);
       $(".page").animate({top: 0}, duration);
   }
+}
+
+function makeEventDiv(name, description, tags, isPublic) {
+  var string = "";
+  string += ' <div class="eventObject space">'
+  string += '   <div class="form-group row">'
+  string += '     <div class="col-sm-2">'
+  string += '       <p class="bg-1">Name:</p>'
+  string += '     </div>'
+  string += '     <div class="col-sm-7">'
+  string += '       <input type="text" class="eventName form-control" value="' + name + '"></input>'
+  string += '     </div>'
+  string += '     <div class="col-sm-2">'
+  string += '       <p class="bg-1">Public:</p>'
+  string += '     </div>'
+  string += '     <div class="col-sm-1">'
+  string += '       <input type="checkbox" class="eventFriends form-control col-sm-1"></input>'
+  string += '     </div>'
+  string += '   </div>'
+  string += '   <div class="form-group row">'
+  string += '     <div class="col-sm-2">'
+  string += '       <p class="bg-1">Tags:</p>'
+  string += '     </div>'
+  string += '     <div class="col-sm-10">'
+  string += '       <input type="text" class="eventTags form-control" value="' + tags + '"></input>'
+  string += '     </div>'
+  string += '   </div>'
+  string += '   <div class="form-group row">'
+  string += '     <div class="col-sm-12">'
+  string += '       <textarea class="eventDescription form-control">' + description + '</textarea>'
+  string += '     </div>'
+  string += '   </div>'
+  string += '   <div class="form-group row">'
+  string += '     <div class="col-sm-4">'
+  string += '       <button type="button" class="btn btn-success btn-block groupChat">Group Chat</button>'
+  string += '     </div>'
+  string += '     <div class="col-sm-4">'
+  string += '       <button type="button" class="btn btn-success btn-block groupChat">View Attendees</button>'
+  string += '     </div>'
+  string += '     <div class="col-sm-4">'
+  string += '       <button type="button" class="btn btn-primary btn-block groupChat">Save Chages</button>'
+  string += '     </div>'
+  string += '   </div>'
+  string += ' </div>'
+
+  return string;
 }

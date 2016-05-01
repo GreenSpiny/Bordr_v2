93f0fe725b788a6fd9b9eef1b7fe8957067e8bbc
chandlermaskal <chandler.maskal@yahoo.com>
2016-05-01 18:18:45 -0400

    Merging

--- Components/Resources/js/transitions.js
+++ Components/Resources/js/transitions.js
@@ -57,11 +57,19 @@ function goToPage(num) {
     scroll("down",750);
   }
   
+  // Hide all info boxes
+  $(".infoArea").each(function() {
+    $(this).hide();
+  });
+  
   // Remove password
   $("#password").val("");
   
   // Set page number to current page
   page = num;
+  
+  // TESTING
+  $("#eventsArea").html(makeEventDiv("Albert's Party","30 years!","skydiving","false"));
 }
 
 function scroll(direction, duration) {
@@ -78,10 +86,48 @@ function scroll(direction, duration) {
   }
 }
 
-function transition(button) {
-  goToPage(button.attr("value"));
-  $(".infoArea").each(function(){
-    //what the hell does this do?!
-    // button.hide();
-  });
+function makeEventDiv(name, description, tags, isPublic) {
+  var string = "";
+  string += ' <div class="eventObject space">'
+  string += '   <div class="form-group row">'
+  string += '     <div class="col-sm-2">'
+  string += '       <p class="bg-1">Name:</p>'
+  string += '     </div>'
+  string += '     <div class="col-sm-7">'
+  string += '       <input type="text" class="eventName form-control" value="' + name + '"></input>'
+  string += '     </div>'
+  string += '     <div class="col-sm-2">'
+  string += '       <p class="bg-1">Public:</p>'
+  string += '     </div>'
+  string += '     <div class="col-sm-1">'
+  string += '       <input type="checkbox" class="eventFriends form-control col-sm-1"></input>'
+  string += '     </div>'
+  string += '   </div>'
+  string += '   <div class="form-group row">'
+  string += '     <div class="col-sm-2">'
+  string += '       <p class="bg-1">Tags:</p>'
+  string += '     </div>'
+  string += '     <div class="col-sm-10">'
+  string += '       <input type="text" class="eventTags form-control" value="' + tags + '"></input>'
+  string += '     </div>'
+  string += '   </div>'
+  string += '   <div class="form-group row">'
+  string += '     <div class="col-sm-12">'
+  string += '       <textarea class="eventDescription form-control">' + description + '</textarea>'
+  string += '     </div>'
+  string += '   </div>'
+  string += '   <div class="form-group row">'
+  string += '     <div class="col-sm-4">'
+  string += '       <button type="button" class="btn btn-success btn-block groupChat">Group Chat</button>'
+  string += '     </div>'
+  string += '     <div class="col-sm-4">'
+  string += '       <button type="button" class="btn btn-success btn-block groupChat">View Attendees</button>'
+  string += '     </div>'
+  string += '     <div class="col-sm-4">'
+  string += '       <button type="button" class="btn btn-primary btn-block groupChat">Save Chages</button>'
+  string += '     </div>'
+  string += '   </div>'
+  string += ' </div>'
+
+  return string;
 }

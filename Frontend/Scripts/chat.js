9bddb3a5cf57350d11342a903ff25928bc112cbe
Conrad Mossl <conradmossl@gmail.com>
2016-05-05 19:04:22 -0400

    Comments!

--- Frontend/Scripts/chat.js
+++ Frontend/Scripts/chat.js
@@ -7,6 +7,9 @@ var app = angular.module('chatApp',[]);
 //add controller to app
 app.controller("chatController",
 function($scope, $http) {
+
+  //this lovely little piece of code parses the relevant information from
+  //url so it can be used by the socket to create the correct and unique chat room
   var url_ = window.location.href;
   var trap = false;
   var eventBool = false;
@@ -38,34 +41,41 @@ function($scope, $http) {
     count--;
   }
 
+  //parsed data is set into variables
   var event = data[0];
   var room;
   var name = data[1];
   var names;
-  console.log(data);
-
   var data = {room: room, event:event, users:names};
+
+  //post request to set up the unique chat room
   $.post('http://localhost:3000/startChat', data).done(function(response) {
+    //unique id of the room
     room = response;
+    //finds the id in the database and creates a chat room with that ID
     socket.emit('room', {room:room});
   }); 
 
   $scope.send = function() {
+    //if the input is not blank
     if (!($scope.words == "" || $scope.words == undefined)) {
+      //send it to the chat room to be broadcast
       socket.emit("send", {room: room, event:event, users: names, message: [{speaker: name, words: $scope.words}] });
     }
   }
 
+  //when a tweet comes in
   socket.on("recieve", function(data){
-    console.log(data);
+    //empty the input box
     $scope.words = "";
     $scope.$apply();
+    //append the new list item to chat window
     var string = "";
-            string += ' <li class="message"> ';
-            string +=   data.speaker;            
-            string +=   ": ";            
-            string +=   data.words;
-            string += ' </li> ';
-        document.getElementById('conversation').innerHTML += string; 
+        string += ' <li class="message"> ';
+        string +=   data.speaker;            
+        string +=   ": ";            
+        string +=   data.words;
+        string += ' </li> ';
+    document.getElementById('conversation').innerHTML += string; 
   });
 });
--- Frontend/Scripts/my_events.js
+++ Frontend/Scripts/my_events.js
@@ -1,19 +1,18 @@
 JS_my_events = {
   Initialize : function() {
   	// Grab event data via POST request
-  	console.log(current_user);
   	var events = current_user.user.hosting_events;
-  	console.log("events",events);
+  	//go through each of the events
   	for (var i=0; i < events.length; i++)
-		{
+		{	
+			//create an acutal object to work with
 			var eventsObject = JSON.parse(events[i]);
-			console.log(eventsObject);
+			//small logic to decide whether or not to check the box or not
 			var checked = "";
-			console.log(eventsObject.private);
-			if (eventsObject.private) {
-				console.log("checked");
+			if ((eventsObject.private == "true") || (eventsObject.private == true)) {
 				checked = "checked";
 			}
+			//create html element
 			var string = "";
 			string += ' <div class="eventObject space">'
 			string += '   <div class="form-group row">'
@@ -57,19 +56,23 @@ JS_my_events = {
 			string += ' </div>'
 			document.getElementById('event-feed').innerHTML += string;
 		}
+	//when any of the group chat buttons are clicked
 	$(".groupChat").on("click", function(event ) {
+		//parse some of the respective data out
 		var data = [0,0];
 		data[0] = event.target.parentNode.parentNode.parentNode.childNodes[1].childNodes[3].childNodes[1].value;
 		data[1] = current_user.user.username;
+		//create a form that sets a get request to the local server
 		var form = document.createElement("form");
 	    form.setAttribute("method", "get");
 	    form.setAttribute("action", "http://localhost:3000/chat");
 	    form.setAttribute("target", "view");
-
+	    //includes attributes
 	    var hiddenField = document.createElement("input"); 
 	    hiddenField.setAttribute("type", "hidden");
 	    hiddenField.setAttribute("name", "message");
 	    hiddenField.setAttribute("value", data);
+	    //activate the form and hide any trace of the firing of the new window
 	    form.appendChild(hiddenField);
 	    document.body.appendChild(form);
 	    window.open('', 'view', 'resizable=0,width=350,height=250');
